bac42b955be98aee39aecdc959ac8e1e8809ce59
Ali Youssef <122641881+AliYoussef97@users.noreply.github.com>
2023-06-12 20:44:27 +0100

    Update download_colmap.bat

--- scripts/download_colmap.bat
+++ scripts/download_colmap.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 REM %~p0 returns the path without the drive. 
 REM %~d0 returns the drive.
@@ -18,6 +19,32 @@ if %errorlevel% EQU 0 (
     echo Deleting Colmap zip file.
     del /f COLMAP-3.8-windows-cuda.zip
 
+    REM Get the current value of the Path variable
+    for /f "usebackq tokens=2,*" %%A in (`reg query "HKEY_CURRENT_USER\Environment" /v PATH`) do set USERPATH=%%B
+    for /f "tokens=2,*" %%A in ('reg query "HKEY_CURRENT_USER\Environment" /v PATH') do (
+    if /i "%%A"=="REG_SZ" (
+        set "pathType=REG_SZ"
+    ) else if /i "%%A"=="REG_EXPAND_SZ" (
+        set "pathType=REG_EXPAND_SZ"
+    )
+    )
+
+    REM Check if the path already exists in the variable
+    echo !USERPATH! | findstr /i "%CD%\\utils\\colmap\\COLMAP-3.8-windows-cuda\\" > nul
+
+    if not errorlevel 1 (
+        REM Path already exists
+        echo Path already exists
+        
+    ) else (
+        
+        REM Append the new path to the existing PATH
+        set "newPath=%CD%\utils\colmap\COLMAP-3.8-windows-cuda\;"
+        set "updatedPath=!USERPATH!!newPath!"
+        reg add "HKEY_CURRENT_USER\Environment" /v PATH /t !pathType! /d "!updatedPath!" /f
+
+    )
+
  
 ) else (
     
@@ -30,6 +57,33 @@ if %errorlevel% EQU 0 (
     echo Deleting Colmap zip file.
     del /f COLMAP-3.8-windows-no-cuda.zip
 
+    REM Get the current value of the Path variable
+    for /f "usebackq tokens=2,*" %%A in (`reg query "HKEY_CURRENT_USER\Environment" /v PATH`) do set USERPATH=%%B
+    for /f "tokens=2,*" %%A in ('reg query "HKEY_CURRENT_USER\Environment" /v PATH') do (
+    if /i "%%A"=="REG_SZ" (
+        set "pathType=REG_SZ"
+    ) else if /i "%%A"=="REG_EXPAND_SZ" (
+        set "pathType=REG_EXPAND_SZ"
+    )
+    )
+
+    REM Check if the path already exists in the variable
+    echo !USERPATH! | findstr /i "%CD%\\utils\\colmap\\COLMAP-3.8-windows-no-cuda\\" > nul
+
+    if not errorlevel 1 (
+        REM Path already exists
+        echo Path already exists
+        
+    ) else (
+        
+        REM Append the new path to the existing PATH
+        set "newPath=%CD%\utils\colmap\COLMAP-3.8-windows-no-cuda\;"
+        set "updatedPath=!USERPATH!!newPath!"
+
+        reg add "HKEY_CURRENT_USER\Environment" /v PATH /t !pathType! /d "!updatedPath!" /f
+    )
+
 )
 
+endlocal
 exit /b
